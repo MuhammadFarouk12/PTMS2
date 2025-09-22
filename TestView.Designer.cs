91db32573ec9937dbb26a807fab472993d02d422
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-09-22 07:43:17 -0700

    Uploading the designer files 2

---
+++ TestView.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace LoginForm
+{
+    partial class TestView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.comboBox_SelectTest = new System.Windows.Forms.ComboBox();
+            this.button_ShowTest = new System.Windows.Forms.Button();
+            this.DataGridView_ShowTest = new Siticone.Desktop.UI.WinForms.SiticoneDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_ShowTest)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_SelectTest
+            // 
+            this.comboBox_SelectTest.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_SelectTest.FormattingEnabled = true;
+            this.comboBox_SelectTest.Location = new System.Drawing.Point(191, 27);
+            this.comboBox_SelectTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox_SelectTest.Name = "comboBox_SelectTest";
+            this.comboBox_SelectTest.Size = new System.Drawing.Size(376, 25);
+            this.comboBox_SelectTest.TabIndex = 0;
+            // 
+            // button_ShowTest
+            // 
+            this.button_ShowTest.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_ShowTest.ForeColor = System.Drawing.Color.Black;
+            this.button_ShowTest.Location = new System.Drawing.Point(573, 26);
+            this.button_ShowTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_ShowTest.Name = "button_ShowTest";
+            this.button_ShowTest.Size = new System.Drawing.Size(106, 24);
+            this.button_ShowTest.TabIndex = 1;
+            this.button_ShowTest.Text = "Show";
+            this.button_ShowTest.UseVisualStyleBackColor = true;
+            this.button_ShowTest.Click += new System.EventHandler(this.button_ShowTest_Click);
+            // 
+            // DataGridView_ShowTest
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.DataGridView_ShowTest.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGridView_ShowTest.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DataGridView_ShowTest.ColumnHeadersHeight = 4;
+            this.DataGridView_ShowTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView_ShowTest.DefaultCellStyle = dataGridViewCellStyle3;
+            this.DataGridView_ShowTest.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.DataGridView_ShowTest.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView_ShowTest.Location = new System.Drawing.Point(0, 56);
+            this.DataGridView_ShowTest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DataGridView_ShowTest.Name = "DataGridView_ShowTest";
+            this.DataGridView_ShowTest.RowHeadersVisible = false;
+            this.DataGridView_ShowTest.RowHeadersWidth = 51;
+            this.DataGridView_ShowTest.RowTemplate.Height = 26;
+            this.DataGridView_ShowTest.Size = new System.Drawing.Size(881, 545);
+            this.DataGridView_ShowTest.TabIndex = 2;
+            this.DataGridView_ShowTest.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView_ShowTest.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.DataGridView_ShowTest.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.DataGridView_ShowTest.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.DataGridView_ShowTest.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.DataGridView_ShowTest.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView_ShowTest.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.DataGridView_ShowTest.ThemeStyle.HeaderStyle.Height = 4;
+            this.DataGridView_ShowTest.ThemeStyle.ReadOnly = false;
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.Height = 26;
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView_ShowTest.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.DataGridView_ShowTest.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_ShowTest_CellContentClick);
+            // 
+            // TestView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 601);
+            this.Controls.Add(this.DataGridView_ShowTest);
+            this.Controls.Add(this.button_ShowTest);
+            this.Controls.Add(this.comboBox_SelectTest);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "TestView";
+            this.Text = "TestView";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TestView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_ShowTest)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_SelectTest;
+        private System.Windows.Forms.Button button_ShowTest;
+        private Siticone.Desktop.UI.WinForms.SiticoneDataGridView DataGridView_ShowTest;
+    }
+}
+
