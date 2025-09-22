873126859d8ca36277e2f09f888c2b0a6d58fc56
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-09-22 07:43:17 -0700

    Uploading the designer files 2

---
+++ PopQuestion.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace LoginForm
+{
+    partial class PopQuestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rd_answer_1 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.rd_answer_3 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.rd_answer_4 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.rd_answer_2 = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.grb_radio = new System.Windows.Forms.GroupBox();
+            this.lbl_answer_4 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.lbl_answer_3 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.lbl_answer_2 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.lbl_answer_1 = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            this.btn_submitQuestion = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.grb_radio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rd_answer_1
+            // 
+            this.rd_answer_1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_1.CheckedState.BorderThickness = 0;
+            this.rd_answer_1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.rd_answer_1.Location = new System.Drawing.Point(6, 61);
+            this.rd_answer_1.Name = "rd_answer_1";
+            this.rd_answer_1.Size = new System.Drawing.Size(20, 20);
+            this.rd_answer_1.TabIndex = 1;
+            this.rd_answer_1.Text = "guna2CustomRadioButton1";
+            this.rd_answer_1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.rd_answer_1.UncheckedState.BorderThickness = 2;
+            this.rd_answer_1.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.rd_answer_1.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // rd_answer_3
+            // 
+            this.rd_answer_3.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_3.CheckedState.BorderThickness = 0;
+            this.rd_answer_3.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_3.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.rd_answer_3.Location = new System.Drawing.Point(6, 210);
+            this.rd_answer_3.Name = "rd_answer_3";
+            this.rd_answer_3.Size = new System.Drawing.Size(20, 20);
+            this.rd_answer_3.TabIndex = 2;
+            this.rd_answer_3.Text = "guna2CustomRadioButton2";
+            this.rd_answer_3.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.rd_answer_3.UncheckedState.BorderThickness = 2;
+            this.rd_answer_3.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.rd_answer_3.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // rd_answer_4
+            // 
+            this.rd_answer_4.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_4.CheckedState.BorderThickness = 0;
+            this.rd_answer_4.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_4.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.rd_answer_4.Location = new System.Drawing.Point(6, 282);
+            this.rd_answer_4.Name = "rd_answer_4";
+            this.rd_answer_4.Size = new System.Drawing.Size(20, 20);
+            this.rd_answer_4.TabIndex = 3;
+            this.rd_answer_4.Text = "guna2CustomRadioButton3";
+            this.rd_answer_4.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.rd_answer_4.UncheckedState.BorderThickness = 2;
+            this.rd_answer_4.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.rd_answer_4.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // rd_answer_2
+            // 
+            this.rd_answer_2.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_2.CheckedState.BorderThickness = 0;
+            this.rd_answer_2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.rd_answer_2.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.rd_answer_2.Location = new System.Drawing.Point(6, 135);
+            this.rd_answer_2.Name = "rd_answer_2";
+            this.rd_answer_2.Size = new System.Drawing.Size(20, 20);
+            this.rd_answer_2.TabIndex = 4;
+            this.rd_answer_2.Text = "guna2CustomRadioButton4";
+            this.rd_answer_2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.rd_answer_2.UncheckedState.BorderThickness = 2;
+            this.rd_answer_2.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.rd_answer_2.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // grb_radio
+            // 
+            this.grb_radio.Controls.Add(this.lbl_answer_4);
+            this.grb_radio.Controls.Add(this.lbl_answer_3);
+            this.grb_radio.Controls.Add(this.lbl_answer_2);
+            this.grb_radio.Controls.Add(this.lbl_answer_1);
+            this.grb_radio.Controls.Add(this.rd_answer_1);
+            this.grb_radio.Controls.Add(this.rd_answer_2);
+            this.grb_radio.Controls.Add(this.rd_answer_3);
+            this.grb_radio.Controls.Add(this.rd_answer_4);
+            this.grb_radio.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grb_radio.Location = new System.Drawing.Point(27, 30);
+            this.grb_radio.Name = "grb_radio";
+            this.grb_radio.Size = new System.Drawing.Size(738, 368);
+            this.grb_radio.TabIndex = 5;
+            this.grb_radio.TabStop = false;
+            this.grb_radio.Text = "Question";
+            // 
+            // lbl_answer_4
+            // 
+            this.lbl_answer_4.AutoSize = false;
+            this.lbl_answer_4.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_answer_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_answer_4.Location = new System.Drawing.Point(51, 282);
+            this.lbl_answer_4.Name = "lbl_answer_4";
+            this.lbl_answer_4.Size = new System.Drawing.Size(681, 60);
+            this.lbl_answer_4.TabIndex = 8;
+            this.lbl_answer_4.Text = "Answer 4";
+            // 
+            // lbl_answer_3
+            // 
+            this.lbl_answer_3.AutoSize = false;
+            this.lbl_answer_3.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_answer_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_answer_3.Location = new System.Drawing.Point(51, 210);
+            this.lbl_answer_3.Name = "lbl_answer_3";
+            this.lbl_answer_3.Size = new System.Drawing.Size(681, 60);
+            this.lbl_answer_3.TabIndex = 7;
+            this.lbl_answer_3.Text = "Answer 3";
+            // 
+            // lbl_answer_2
+            // 
+            this.lbl_answer_2.AutoSize = false;
+            this.lbl_answer_2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_answer_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_answer_2.Location = new System.Drawing.Point(51, 127);
+            this.lbl_answer_2.Name = "lbl_answer_2";
+            this.lbl_answer_2.Size = new System.Drawing.Size(681, 60);
+            this.lbl_answer_2.TabIndex = 6;
+            this.lbl_answer_2.Text = "Answer 2";
+            // 
+            // lbl_answer_1
+            // 
+            this.lbl_answer_1.AutoSize = false;
+            this.lbl_answer_1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_answer_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_answer_1.Location = new System.Drawing.Point(51, 61);
+            this.lbl_answer_1.Name = "lbl_answer_1";
+            this.lbl_answer_1.Size = new System.Drawing.Size(681, 60);
+            this.lbl_answer_1.TabIndex = 5;
+            this.lbl_answer_1.Text = "Answer 1";
+            // 
+            // btn_submitQuestion
+            // 
+            this.btn_submitQuestion.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_submitQuestion.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_submitQuestion.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_submitQuestion.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_submitQuestion.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_submitQuestion.ForeColor = System.Drawing.Color.White;
+            this.btn_submitQuestion.Location = new System.Drawing.Point(303, 420);
+            this.btn_submitQuestion.Name = "btn_submitQuestion";
+            this.btn_submitQuestion.Size = new System.Drawing.Size(180, 45);
+            this.btn_submitQuestion.TabIndex = 6;
+            this.btn_submitQuestion.Text = "Submit";
+            this.btn_submitQuestion.Click += new System.EventHandler(this.btn_submitQuestion_Click);
+            // 
+            // PopQuestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 492);
+            this.Controls.Add(this.btn_submitQuestion);
+            this.Controls.Add(this.grb_radio);
+            this.Name = "PopQuestion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PopQuestion";
+            this.Load += new System.EventHandler(this.PopQuestion_Load);
+            this.grb_radio.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomRadioButton rd_answer_1;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton rd_answer_3;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton rd_answer_4;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton rd_answer_2;
+        private System.Windows.Forms.GroupBox grb_radio;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel lbl_answer_4;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel lbl_answer_3;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel lbl_answer_2;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel lbl_answer_1;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btn_submitQuestion;
+    }
+}
+
